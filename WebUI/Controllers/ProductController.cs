629e87975cdbf897d87af0c7c8a096d57c9d07a2
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2021-04-06 01:03:03 +0300

    crud operations ok.

--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Business.Abstract;
 using Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebUI.Controllers
 {
@@ -22,5 +23,92 @@ namespace WebUI.Controllers
         {
             return View(await _productService.GetList());
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var product = await _productService.GetById((int)id);
+            if (product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var p = await _productService.GetById(id);
+            await _productService.Delete(p);
+            return RedirectToAction("Index");
+        }
+      
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            var product = await _productService.GetById((int)id);
+            if (product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            var product = await _productService.GetById((int)id);
+            if (product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+
     }
 }

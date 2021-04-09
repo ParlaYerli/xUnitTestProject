3e1cb87269a6a90a5005f13ff8f8b61e4612d216
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2021-04-09 13:09:05 +0300

    add test for create action

--- XUnitTest.Test/ProductControllerTest.cs
+++ XUnitTest.Test/ProductControllerTest.cs
@@ -58,7 +58,7 @@ namespace xUnitTest.Test
             _mockService.Setup(x => x.GetById(0)).ReturnsAsync(product);
             var result = await _controller.Details(0);
             var redirect = Assert.IsType<NotFoundResult>(result);
-            Assert.Equal<int>(404,redirect.StatusCode);
+            Assert.Equal<int>(404, redirect.StatusCode);
         }
         [Theory]
         [InlineData(1)]
@@ -69,7 +69,44 @@ namespace xUnitTest.Test
             var result = await _controller.Details(productId);
             var viewResult = Assert.IsType<ViewResult>(result);
             var resultProduct = Assert.IsAssignableFrom<Product>(viewResult.Model);
-            Assert.Equal(product.Id,resultProduct.Id);
-        }   
+            Assert.Equal(product.Id, resultProduct.Id);
+        }
+        [Fact]
+        public void Create_ActionExecutes_ReturnView()
+        {
+            var result = _controller.Create();
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public async void CreatePOST_ModalStateInValid_ReturnView()
+        {
+            _controller.ModelState.AddModelError("Name", "The Name field is required");
+            var result = await _controller.Create(list.First());
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<Product>(viewResult.Model);
+        }
+        [Fact]
+        public async void CreatePOST_ModalStateValid_ReturnRedirectToIndexAction()
+        {
+            var result = await _controller.Create(list.First());
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+        }
+        [Fact]
+        public async void CreatePOST_ModalStateValid_CreateMethodExecute()
+        {
+            Product product = null;
+            _mockService.Setup(x => x.Add(It.IsAny<Product>())).Callback<Product>(x => product = x);
+            var result = await _controller.Create(list.First());
+            _mockService.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
+            Assert.Equal(list.First().Id, product.Id);
+        }
+        [Fact]
+        public async void CreatePOST_ModalStateInValid_NeverAddExecute()
+        {
+            _controller.ModelState.AddModelError("Name", "");
+            var result = await _controller.Create(list.First());
+            _mockService.Verify(x => x.Add(It.IsAny<Product>()), Times.Never);
+        }
     }
 }
